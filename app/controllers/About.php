7c79358c07d5967ce5368bf1d55cffe742785c5a
hilmanaji <hilman17aji@gmail.com>
2019-08-28 11:46:27 +0700

    Kerangka MVC
    Create Kerangka MVC
    - Konfigurasi file Htaacess
    - Konfigurasii URL
    - Configurasi Method dan parameter

---
+++ app/controllers/About.php
@@ -0,0 +1,11 @@
+<?php
+
+class About {
+    public function index($nama = "hilman", $pekerjaan = "Gamer") 
+    {
+        echo "Hallo, nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+    public function page() {
+
+    }
+}
