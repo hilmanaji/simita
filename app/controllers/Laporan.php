654c6aa375943e7064364596becfbbd04a346bcd
hilmanaji <hilman17aji@gmail.com>
2019-12-18 10:29:17 +0700

    Laporan
    insert data laporan

--- app/controllers/Laporan.php
+++ app/controllers/Laporan.php
@@ -3,7 +3,14 @@ class Laporan extends Controller {
     public function index() {
         $data['judul'] = 'Laporan';
 		$data['sub_judul'] = 'Cetak Laporan';
-        
+		
+		if ($_SESSION["role_user"] == 'Admin Project') {
+			$data['data_project'] = $this->model('DataHandle')->getProject();
+
+		}else {
+			$id_mitra = $_SESSION['id_mitra'];
+			$data['data_project'] = $this->model('DataHandle')->getProjectById($id_mitra);
+		}
         
 		$this->view('templates/header', $data);
 		$this->view('templates/sidebar', $data);
--- app/views/laporan/index.php
+++ app/views/laporan/index.php
@@ -6,6 +6,45 @@
     <div class="navigasi">
     <a href="<?= BASEURL; ?>/Laporan/getData/">PRINT PROJECT</a>
     </div>
+    <div class="table-wrapper">
+        <table class="fl-table">
+            <thead>
+            <tr>
+                <th>NO</th>
+                <th>ID PROJECT</th>
+                <th>NO PO</th>
+                <th>DATEL</th>
+                <th>STO</th>
+                <th>SITE NAME</th>
+                <th>ODP</th>
+                <th>PORT</th>
+                <th>TOC</th>
+                <th>MATERIAL</th>
+                <th>JASA</th>
+                <th>TOTAL</th>                
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $no = 1; ?>
+            <?php foreach ( $data['data_project'] as $project ) : ?>
+            <tr>
+                <td><?= $no++; ?></td>
+                <td><?= $project['id_project'] ?></td>
+                <td><?= $project['no_po'] ?></td>
+                <td><?= $project['datel'] ?></td>
+                <td><?= $project['nama_sto'] ?></td>
+                <td><?= $project['nama_lokasi'] ?></td>
+                <td><?= $project['jumlah_odp'] ?></td>
+                <td><?= $project['jumlah_port'] ?></td>
+                <td><?= $project['toc'] ?></td>
+                <td><?= $project['nilai_material'] ?></td>
+                <td><?= $project['nilai_jasa'] ?></td>
+                <td><?= $project['total'] ?></td>
+            </tr>
+            <?php endforeach; ?>
+            <tbody>
+        </table>
     <form action="">    
         <!-- <label for="data">
             <span>TIPE USER</span>
